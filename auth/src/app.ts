1c04275c9e680dbe2002a95e985477e4c911e3ce
wizli595 <abdoowizli@gmail.com>
2024-02-09 06:32:21 +0100

    .log file

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -5,6 +5,9 @@ import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
 import { errorHandler, NotFoundError } from '@wizlitickets/common';
+import fs from 'fs';
+import path from 'path';
+import morgan from 'morgan';
 
 import cookieSession from 'cookie-session';
 
@@ -13,6 +16,10 @@ const app = express()
 const PORT = 3000
 
 app.set('trust proxy', true);
+
+const accessLogStrem = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStrem }))
+console.log(path.join(__dirname, 'access.log'));
 app.use(express.json())
 app.use(cookieSession({
     signed: false,
@@ -25,7 +32,7 @@ app.use("/api/users", signOutRouter)
 app.use("/api/users", signUpRouter)
 
 
-app.all("*", async () => {
+app.all("*", async (req, res) => {
     throw new NotFoundError();
 })
 
